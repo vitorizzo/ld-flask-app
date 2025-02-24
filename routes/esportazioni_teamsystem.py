3a8cb19bed81bb88d3316db4ebf0a987e6bea79f
vitorizzo <vito.rizzo74@tim.it>
2025-02-24 18:34:01 +0100

    Implementazione erogazione file estratti dal gestionale. - 9

--- routes/esportazioni_teamsystem.py
+++ routes/esportazioni_teamsystem.py
@@ -123,7 +123,7 @@ def serve_risorsa(filename):
 
     if os.path.exists(local_file_path):
         print(f"restituisco il file locale: {local_file_path}")
-        return local_file_path
+        return get_exported_file(filename)
     else:
         # Scarica il file remoto
         print(f"restituisco il file remoto: {remote_file_url}")
