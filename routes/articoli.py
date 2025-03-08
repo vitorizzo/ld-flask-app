a374984d07673f4d99cea7ddda192cf56ccbdcad
vitorizzo <vito.rizzo74@tim.it>
2025-03-08 18:30:24 +0100

    Implementazione ricerca per descrizione

--- routes/articoli.py
+++ routes/articoli.py
@@ -6,4 +6,4 @@ articoli_bp = Blueprint('articoli', __name__, template_folder='../templates')
 
 @articoli_bp.route('/articoli')
 def articoli():
-    return render_template('articoli.html')
+    return render_template('articoli_codebar.html')
--- routes/search.py
+++ routes/search.py
@@ -7,7 +7,12 @@ search_bp = Blueprint('search', __name__, template_folder='../templates')
 
 @search_bp.route('/ricerca_x_barcode', methods=['GET', 'POST'])
 def ricerca_x_barcode():
-    return render_template('articoli.html')
+    return render_template('articoli_codebar.html')
+
+
+@search_bp.route('/ricerca_x_descrizione', methods=['GET', 'POST'])
+def ricerca_x_descrizione():
+    return render_template('articoli_description.html')
 
 
 @search_bp.route('/articolo_by_barcode', methods=['GET'])
@@ -38,3 +43,49 @@ def articolo_per_barcode():
     else:
         print("Articolo non trovato")
         return jsonify({'success': False, 'error': 'Prodotto non trovato.'})
+
+
+@search_bp.route('/lista_articoli', methods=['GET'])
+def lista_articoli():
+    filtro = request.args.get('filter', '').strip()
+    page = request.args.get('page', 1, type=int)  # Numero della pagina, di default 1
+    per_page = request.args.get('per_page', 10, type=int)  # Elementi per pagina, default 10
+
+    query = Articoli.query
+
+    # Filtraggio per descrizione e descrizione_aggiuntiva
+    if filtro:
+        query = query.filter(
+            (Articoli.descrizione.ilike(f"%{filtro}%")) |
+            (Articoli.descrizione_aggiuntiva.ilike(f"%{filtro}%"))
+        )
+
+    # Paginazione
+    paginated = query.paginate(page=page, per_page=per_page, error_out=False)
+
+    prodotti_json = []
+
+    for p in paginated.items:
+        giacenza = Giacenza.query.filter_by(cod_art=p.cod_art).first()  # Recupera giacenza
+
+        prodotto = {
+            'codice': p.cod_art,
+            'descrizione': p.descrizione,
+            'descrizione_aggiuntiva': p.descrizione_aggiuntiva,
+            'prezzo': p.prezzo,
+            'giacenza': {
+                'instore': giacenza.giac_neg if giacenza else 0,  # Se giacenza è None, assegna 0
+                'online': giacenza.giac_www if giacenza else 0,
+                'isonline': True if giacenza and giacenza.giac_www > 0 else False
+            }
+        }
+
+        prodotti_json.append(prodotto)
+
+    return jsonify({
+        'prodotti': prodotti_json,
+        'pagina_corrente': paginated.page,
+        'pagine_totali': paginated.pages,
+        'totale_prodotti': paginated.total,
+        'per_page': per_page
+    })
