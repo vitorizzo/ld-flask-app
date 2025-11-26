5b6c3958024407b2755cf6f97ea41386f00bb523
vitorizzo <vito.rizzo74@tim.it>
2025-11-26 12:10:36 +0100

    fix 2 (eliminazione cache browser) - stabilizzazione sistema login e gestione ruoli

--- app.py
+++ app.py
@@ -114,6 +114,14 @@ def service_worker():
     return response
 
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+
 logger.info("Blueprint registrati.")
 # debug_loggers()
 
