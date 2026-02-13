d08a7d40b80bde305abcf788813ac2d0c9e425dd
vitorizzo <vito.rizzo74@tim.it>
2026-02-13 10:02:35 +0100

    implementazione agenda - creazione route day and type in console fix #2

--- static/js/agenda.js
+++ static/js/agenda.js
@@ -1,5 +1,5 @@
-const day = await fetch("/cassa/api/day", { credentials: "same-origin" });
+const day = fetch("/cassa/api/day", { credentials: "same-origin" });
     if (!res.ok) throw new Error("get_menu_structure failed");
-    return await day.json();
+    return day.json();
 console.log("giorno da backend")
 console.log(day)
