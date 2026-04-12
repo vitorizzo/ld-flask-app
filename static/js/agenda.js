cda4416bcf0060b6f728f7127898a0cdb2f40077
vitorizzo <vito.rizzo74@tim.it>
2026-04-12 10:42:06 +0200

    correzione bugs fix #6 - modale spese fix #10

--- static/js/agenda.js
+++ static/js/agenda.js
@@ -2193,7 +2193,7 @@ function setPaymentMode(mode) {
     }
 
     if (posMoveDocRefSelect) {
-      posMoveDocRefSelect.value = "";
+      posMoveDocRefSelect.value = "Corrispettivi";
     }
 
     if (posMoveNotesInput) {
